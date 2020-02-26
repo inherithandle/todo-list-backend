81ca9c50d214c5d4e0f01919757241af9dcaa81e
inherithandle <inherithandle@gmail.com>
2020-02-26 23:42:18 +0900

    separated logback configuration based on @Profile.

--- src/main/java/com/gtchoi/todolistbackend/config/RequestLoggingFilterConfig.java
+++ src/main/java/com/gtchoi/todolistbackend/config/RequestLoggingFilterConfig.java
@@ -2,9 +2,11 @@ package com.gtchoi.todolistbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
+@Profile("local")
 public class RequestLoggingFilterConfig {
 
     @Bean
